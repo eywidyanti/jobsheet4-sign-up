0adfbe6177fa85b66b4d7f7e6056f5fb420c85a8
Eka Yulianita Widyanti <eywidyanti@gmail.com>
2023-03-08 10:39:57 +0700

    update

---
+++ lib/widget/signupapp.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'signupscreen.dart';
+import 'welcomescreen.dart';
+
+class SignUpApp extends StatelessWidget {
+  const SignUpApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        '/': (context) => const SignUpScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/widget/signupscreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'signupform.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[200],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
